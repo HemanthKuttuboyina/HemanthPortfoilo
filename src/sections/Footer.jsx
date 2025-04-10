26009d7c909e1ab3f4b07ccc3106947a2453f126
Hemanth Kuttuboyina <khemu4567@gmail.com>
2025-04-10 23:26:23 +0530

    added the x linked-in github links

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,28 +8,28 @@ function Footer() {
       <div className="min-h-[100px] flex flex-col justify-center gap-6 text-center text-lg text-gray-300 font-mono mb-10 ">
         <div className="flex gap-10 p-4 justify-center text-white ">
           <a
-            href="https://twitter.com/yourprofile"
+            href="https://x.com/hemu__07?s=11"
             className="border p-2 text-2xl rounded hover:-translate-y-2 transition-all bg-black text-white "
             target="_blank"
           >
             <FaXTwitter />
           </a>
           <a
-            href="https://twitter.com/yourprofile"
+            href="https://www.linkedin.com/in/hemanthk7/"
             className="border p-2 text-2xl rounded hover:-translate-y-2 transition-all bg-blue-700 text-white "
             target="_blank"
           >
             <SlSocialLinkedin />
           </a>
           <a
-            href="https://twitter.com/yourprofile"
+            href="https://github.com/HemanthKuttuboyina"
             className="border p-2 text-2xl rounded hover:-translate-y-2 transition-all bg-gray-800 text-white "
             target="_blank"
           >
             <FaSquareGithub />
           </a>
         </div>
-        <p>@ 2025 Ishaq. All rights reserved. </p>
+        <p>@ 2025 Hemanth all right reserved </p>
       </div>
     </footer>
   );
